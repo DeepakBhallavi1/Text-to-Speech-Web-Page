79abf0aee2a8a46b4befd3db005163dfccd424dc
Deepak Bhallavi <111530825+DeepakBhallavi1@users.noreply.github.com>
2023-07-29 12:06:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ voices();
 
 function voices(){
     for(let voice of synth.getVoices()){
-        let selected = voice.name === "Select" ? "selected" : "";
+        let selected = voice.name === "Google US" ? "selected" : "";
         let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
         voiceList.insertAdjacentHTML("beforeend", option);
     }
